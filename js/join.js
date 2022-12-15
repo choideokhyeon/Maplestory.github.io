a743eccb1df571d4e3e979f252fd62a285830ff0
choideokhyeon <108784393+choideokhyeon@users.noreply.github.com>
2022-12-15 17:26:18 +0900

    Add files via upload

--- js/join.js
+++ js/join.js
@@ -5,13 +5,29 @@ const authinput = document.getElementById('authnum');
 
 const authokbtn = document.querySelector('.authok-btn');
 
+const resetbtn = document.querySelector('.reset-btn');
+
 const cancelbtn = document.querySelector('.cancel-btn');
 
-authbtn.addEventListener('click',function(){
+const inputslot = document.getElementsByClassName('inputslot');
+
+
+//인풋창
+
+authbtn.addEventListener('click', function () {
   alert('전화번호 인증 완료');
   authinput.disabled = false;
 });
 
-cancelbtn.addEventListener('click',function(event){
+resetbtn.addEventListener('click', function () {
+  alert('작성된 내용을 초기화 합니다');
+  for (let i = 0; i < inputslot.length; i++) {
+    console.log(inputslot[i]);
+    console.log(inputslot[i].value);
+    inputslot[i].value = null;
+  }
+})
+
+cancelbtn.addEventListener('click', function (event) {
   alert('취소합니다');
 })
